0d239b28007892885a5a3b2db88f4796fe53fa26
chenyunshu2 <chenyunshu2@gmail.com>
2024-07-01 22:24:47 +0800

    feat: setup server

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,28 +1,71 @@
-import {Application, json, urlencoded, request, NextFunction} from 'express'
-import http from 'http';
+import { Application, json, urlencoded, request, NextFunction } from "express";
+import http from "http";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import compression from "compression";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import "express-async-errors";
+
+const SERVER_PORT = 5000;
 
 export class Server {
-    private app: Application;
+  private app: Application;
 
-    constructor(app: Application) {
-        this.app = app;
-    }
+  constructor(app: Application) {
+    this.app = app;
+  }
 
-    public start(): void{ 
-        this.securityMiddleware(this.app);
-        this.standardMiddleware(this.app);
-        this.routesMiddleware(this.app);
-        this.globalErrorHandler(this.app)
-        this.startServer(this.app);
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["key1", "key2"],
+        maxAge: 24 * 7 * 3600000,
+        sameSite: "none", // comment this line when running the server locally
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        //origin: config.CLIENT_URL,
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
+
+  private routesMiddleware(app: Application): void {}
+  private globalErrorHandler(app: Application): void {}
+  private startServer(app: Application): void {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      //log.error(error);
     }
-    
-    private securityMiddleware(app: Application): void { }
-    
-    private standardMiddleware(app: Application): void { }
-    private routesMiddleware(app: Application): void { }
-    private globalErrorHandler(app: Application): void { }
-    private startServer(app: Application): void { }
-    private createSocketIO(httpServer: http.Server): void { }
-    private startHttpServer(httpServer: http.Server): void { }
-    
+  }
+  private createSocketIO(httpServer: http.Server): void {}
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.log(`Server running on port ${SERVER_PORT}`);
+    });
+  }
 }
