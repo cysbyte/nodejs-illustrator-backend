ed52f53f68e36ff2afc160edf7a242c59540449f
chenyunshu2 <chenyunshu2@gmail.com>
2024-07-23 16:23:33 +0800

    feat: save post to redis cache

--- src/features/post/interfaces/post.interface.ts
+++ src/features/post/interfaces/post.interface.ts
@@ -13,18 +13,15 @@ interface IReactions {
 export interface IPostDocument extends Document {
   _id?: string | mongoose.Types.ObjectId;
   userId: string;
-  authId: string;
-  // username: string;
-  // email: string;
-  // avatarColor: string;
-  // profilePicture: string;
+  username: string;
+  email: string;
+  avatarColor: string;
+  profilePicture: string;
   post: string;
   bgColor: string;
   commentsCount: number;
   imgVersion?: string;
   imgId?: string;
-  videoId?: string;
-  videoVersion?: string;
   feelings?: string;
   gifUrl?: string;
   privacy?: string;
